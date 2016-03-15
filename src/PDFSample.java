02277195263cdf67c6f9560adfdde5625738969b
shiwangi <shiwangi@shiwangi-Lenovo-Z50-70.(none)>
2016-03-15 14:28:30 +0530

    Merged changes

--- src/PDFSample.java
+++ src/PDFSample.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class PDFSample {
 
     // Page configuration
-    private static final PDRectangle PAGE_SIZE =  PDPage.PAGE_SIZE_A2;
+    private static final PDRectangle PAGE_SIZE = PDPage.PAGE_SIZE_A2;
     private static final float MARGIN = 20;
     private static final boolean IS_LANDSCAPE = false;
 
@@ -19,37 +19,35 @@ public class PDFSample {
 
     // Table configuration
     private static final float CELL_MARGIN = 2;
-    
 
 
     public static Table createContent(List<String> heading, String[][] content) {
         // Total size of columns must not be greater than table width.
         List<Column> columns = new ArrayList<Column>();
-        int colWidth = (int) ((PAGE_SIZE.getWidth()-2*MARGIN)/heading.size());
-        for(String head:heading) {
+        int colWidth = (int) ((PAGE_SIZE.getWidth() - 2 * MARGIN) / heading.size());
+        for (String head : heading) {
 
             columns.add(new Column(head, colWidth));
         }
 
 
-
         float tableHeight = IS_LANDSCAPE ? PAGE_SIZE.getWidth() - (2 * MARGIN) : PAGE_SIZE.getHeight() - (2 * MARGIN);
 
-         final float ROW_HEIGHT = tableHeight/(content.length+10);
+        final float ROW_HEIGHT = tableHeight / (content.length + 10);
         Table table = new TableBuilder()
-            .setCellMargin(CELL_MARGIN)
-            .setColumns(columns)
-            .setContent(content)
-            .setHeight(tableHeight)
-            .setNumberOfRows(content.length+5)
-            .setRowHeight(ROW_HEIGHT)
-            .setMargin(MARGIN)
-            .setPageSize(PAGE_SIZE)
-            .setLandscape(IS_LANDSCAPE)
-            .setTextFont(TEXT_FONT)
-            .setFontSize(FONT_SIZE)
-            .build();
-       return table;
+                .setCellMargin(CELL_MARGIN)
+                .setColumns(columns)
+                .setContent(content)
+                .setHeight(tableHeight)
+                .setNumberOfRows(content.length + 5)
+                .setRowHeight(ROW_HEIGHT)
+                .setMargin(MARGIN)
+                .setPageSize(PAGE_SIZE)
+                .setLandscape(IS_LANDSCAPE)
+                .setTextFont(TEXT_FONT)
+                .setFontSize(FONT_SIZE)
+                .build();
+        return table;
 
-    }   
+    }
 }
